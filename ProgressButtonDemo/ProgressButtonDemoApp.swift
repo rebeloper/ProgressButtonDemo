168b2de43045b8750d56aac8f2e51602b4232fa7
rebeloper <therebeloper@gmail.com>
2023-02-05 08:05:05 +0200

    Initial Commit

---
+++ ProgressButtonDemo/ProgressButtonDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProgressButtonDemoApp.swift
+//  ProgressButtonDemo
+//
+//  Created by Alex Nagy on 05.02.2023.
+//
+
+import SwiftUI
+
+@main
+struct ProgressButtonDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
